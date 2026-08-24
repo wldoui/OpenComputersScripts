435c1e72b8f7b95f776b44261b23d69df127e647
Wyldi <Wylfi0485@protonmail.com>
2026-08-24 09:01:14 +0300

    Pixel Test?

--- Test/main.lua
+++ Test/main.lua
@@ -1,10 +1,14 @@
 local component = require("component")
+local gpu = component.gpu
 
-print("=== COMPONENTS ===")
-print()
+gpu.fill(1, 1, 80, 25, " ")
 
-for address, componentType in component.list() do
-    print(componentType)
-    print("  " .. address)
-    print()
+gpu.set(1, 1, "==============================")
+gpu.set(1, 2, "      ENERGY CONTROL")
+gpu.set(1, 3, "==============================")
+
+gpu.set(1, 5, "System started.")
+
+while true do
+    os.sleep(1)
 end
